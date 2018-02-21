0c0a17d51115a5fd99e3e76f61a24f41bd2328a0
Dinh Tan Thong <thong.dt@php-thongdt.sutrixmedia1.com>
2018-02-21 15:23:35 +0700

    update

---
+++ app/src/main/java/com/example/thongdt/okhttp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.thongdt.okhttp;
+
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private NetworkChangeReceiver receiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        receiver = new NetworkChangeReceiver();
+        final IntentFilter filter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(receiver,filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
+    }
+}
