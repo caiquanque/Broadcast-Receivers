1a9ae09ce060d9f648e789d1044b258a0ccd3567
Dinh Tan Thong <thong.dt@php-thongdt.sutrixmedia1.com>
2018-02-21 17:43:15 +0700

    first commit

--- app/src/main/java/com/example/thongdt/okhttp/MainActivity.java
+++ app/src/main/java/com/example/thongdt/okhttp/MainActivity.java
@@ -1,24 +1,70 @@
 package com.example.thongdt.okhttp;
 
-import android.content.IntentFilter;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    private NetworkChangeReceiver receiver;
+    int notificationId =113;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        receiver = new NetworkChangeReceiver();
-        final IntentFilter filter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
-        registerReceiver(receiver,filter);
+        ButterKnife.bind(this);
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        unregisterReceiver(receiver);
+    @OnClick(R.id.btnTao)
+    public void btnTao() {
+        taoNotification();
+    }
+
+    @OnClick(R.id.btnDong)
+    public void btnDong() {
+        dongNotification();
+    }
+
+    private void dongNotification() {
+        NotificationManager mNotifyMgr=
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.cancel(notificationId);
+    }
+
+    private void taoNotification() {
+
+        //Khởi tạo Notification
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.ken)
+            .setContentTitle("Co thong bao")
+            .setContentText("Moi ban cap nhat version");
+
+        Intent resultIntent = new Intent(this, UpdateActivity.class);
+
+        //Khởi tạo pendingInendingIntent
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                resultIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+        );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        mBuilder.setSound(uri);
+
+        //Gọi Notification Service
+        NotificationManager mNotifyMgr=
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(notificationId, mBuilder.build());
     }
 }
